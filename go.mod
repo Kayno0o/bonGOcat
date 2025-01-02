d04f455b826c585ff70bc8167467f1617f2f6278
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-01-02 08:48:50 +0100

    feat: handle click + average key press per second

--- go.mod
+++ go.mod
@@ -2,17 +2,18 @@ module kaynooo.xyz/go-cat
 
 go 1.22.1
 
-require github.com/hajimehoshi/ebiten/v2 v2.6.6
-
-require github.com/vcaesar/keycode v0.10.1 // indirect
+require github.com/hajimehoshi/ebiten/v2 v2.8.6
 
 require (
-	github.com/ebitengine/purego v0.6.0 // indirect
-	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/ebitengine/gomobile v0.0.0-20241016134836-cc2e38a7c0ee // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/vcaesar/keycode v0.10.1 // indirect
+)
+
+require (
+	github.com/ebitengine/purego v0.8.1 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/robotn/gohook v0.41.0
-	golang.org/x/exp/shiny v0.0.0-20230817173708-d852ddb80c63 // indirect
-	golang.org/x/image v0.12.0 // indirect
-	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"image/color"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -28,11 +29,17 @@ var (
 
 	resetChan = make(chan bool)
 
-	position = 0
+	position        = 0
+	keyPresses      int
+	mutex           sync.Mutex
+	calculateTicker = time.NewTicker(time.Second)
+
+	// Sliding window to store keypress counts
+	keypressCounts = make([]int, 5)
+	currentIndex   = 0
 )
 
-type Game struct {
-}
+type Game struct{}
 
 func timer(duration time.Duration, resetChan chan bool) {
 	ticker := time.NewTicker(duration)
@@ -49,6 +56,21 @@ func timer(duration time.Duration, resetChan chan bool) {
 }
 
 func keypress(resetChan chan bool) {
+	hook.Register(hook.MouseDown, nil, func(e hook.Event) {
+		if !registered {
+			return
+		}
+
+		// Handle mouse clicks
+		if e.Button == 1 {
+			position = 1 // Left click sets the position to left
+		} else if e.Button == 3 {
+			position = -1 // Right click sets the position to right
+		}
+		resetChan <- true
+		registered = false
+	})
+
 	hook.Register(hook.KeyDown, nil, func(e hook.Event) {
 		if !registered {
 			return
@@ -60,11 +82,39 @@ func keypress(resetChan chan bool) {
 			position = 1
 		}
 		resetChan <- true
+
+		mutex.Lock()
+		keyPresses++
+		mutex.Unlock()
+
 		registered = false
 	})
 
 	s := hook.Start()
 	<-hook.Process(s)
+}
+
+func calculateAverageKPS() {
+	for range calculateTicker.C {
+		mutex.Lock()
+		// Update sliding window with the latest keypress count
+		keypressCounts[currentIndex] = keyPresses
+		currentIndex = (currentIndex + 1) % len(keypressCounts)
+
+		// Reset the current second's keypress count
+		keyPresses = 0
+		mutex.Unlock()
+	}
+}
+
+func getAverageKPS() float64 {
+	mutex.Lock()
+	defer mutex.Unlock()
+	total := 0
+	for _, count := range keypressCounts {
+		total += count
+	}
+	return float64(total) / float64(len(keypressCounts))
 }
 
 func (g *Game) Update() error {
@@ -85,6 +135,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op.GeoM.Scale(ratio, ratio)
 	screen.DrawImage(img, op)
 	registered = true
+
+	// Draw the average on the top-right corner as plain text
+	average := strconv.FormatFloat(getAverageKPS(), 'f', 2, 64)
+	ebitenutil.DebugPrintAt(screen, average, int(float64(bw)*ratio)-50, 10)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -114,6 +168,7 @@ func main() {
 
 	go timer(time.Second, resetChan)
 	go keypress(resetChan)
+	go calculateAverageKPS()
 
 	left = readImg("assets/left.png")
 	middle = readImg("assets/middle.png")
